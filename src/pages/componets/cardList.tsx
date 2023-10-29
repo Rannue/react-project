2e4c2fe2dee14524a43f3c35d6807acb39538cd0
Anna <yavianbo@gmail.com>
2023-10-29 18:54:49 +0300

    feat: add content components (cardItem, cardList, spinner)

---
+++ src/pages/componets/cardList.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Character } from '../home-page';
+import CardItem from './cardItem';
+
+interface CardsProps {
+  characters: Character[] | null;
+}
+
+const CardList: React.FC<CardsProps> = ({ characters }) => {
+  if (Array.isArray(characters)) {
+    return (
+      <div className="card-container">
+        {characters.map((card) => (
+          <div className="card" key={card.id}>
+            <CardItem character={card} />
+          </div>
+        ))}
+      </div>
+    );
+  }
+};
+
+export default CardList;
---
+++ src/pages/componets/spinner.tsx
@@ -0,0 +1,14 @@
+const Spinner = () => {
+  return (
+    <>
+      <div className="loading-dots" data-testid="spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <h1>OOOOOOOOOOOOOOOOOOOOOOOOOOOO</h1>
+      </div>
+    </>
+  );
+};
+
+export default Spinner;
